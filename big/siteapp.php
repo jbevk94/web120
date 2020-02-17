ca09923641a5767aba1de4a052a03ccf0bd0dceb
Beverly James <jbevk94@gmail.com>
2020-02-17 15:23:37 -0800

    Added Site vs App Page

---
+++ big/siteapp.php
@@ -0,0 +1,33 @@
+<?php include "includes/header.php"?>
+
+<h1>Responsive Websites vs Mobile Application</h1>
+<p>
+There was a point in time where just having a basic website was enough to promote your business, but now when technology has advanced so much it’s not so simple. Before it was expected that if people were looking at your website they were doing so from their desktop computer. Now there are laptops, tablets, and cell phones to take into consideration as well. Sure, the website you design will still be accessible on smaller screens but it won’t look or function as well. 
+</p>
+<p>
+The point of a responsive website is to ensure that whatever device the user is on, they are able to view the content as it was intended. With the use of CSS media queries, flexible layouts, and flexible media, responsive design is made possible. Using breakpoints, a developer is able to change the view of the website so that the design changes to match whatever device is being used at the time. Essentially, it’s one website that may look somewhat different on each different screen size.
+</p>
+<p>
+Billions of people access the internet every single day and almost half of the users that view web pages, do so from their mobile devices. (Gordon) Many people don’t even own computers and their cell phone is their main source of accessing the internet. This goes to show how important it is to have your webpage show up correctly on smaller screens. Mobile applications are specifically designed for cell phones and tablets.
+</p>
+<p>
+There are many pros and cons when it comes to deciding which one is better and I believe it depends on the purpose of what you are trying to accomplish. Responsive websites are generally less expensive and take less time to create. (Kris) The process of uploading and fixing bugs is fairly easy, but the websites are fairly slower than mobile apps. Mobile applications, on the other hand, don’t always require the internet to access and are incredibly user-friendly. (ThinkMobiles) They work really well with hardware sensors such as microphones and cameras. Unfortunately, mobile apps are not compatible with all devices so a device designed for an android won’t work on ios. It’s important to understand what the client is looking for in order to decide which one would be best to use. 
+
+</p>
+</section>
+<!-- End left col -->
+
+<!-- Start right col -->
+<aside>
+<h2>Citations</h2>
+<p>
+Gordon, Kyle. “Topic: Mobile Internet Usage Worldwide.” Www.statista.com, 11 Sept. 2019, www.statista.com/topics/779/mobile-internet/.
+
+Kris. “The Pros and Cons of Mobile Apps vs. Responsive Web Design.” Smashstack, 5 Jan. 2017, www.smashstack.com/articles/the-pros-and-cons-of-mobile-apps-vs-responsive-web-design/.
+
+“Responsive Website vs Mobile App: Comparison.” Thinkmobiles, thinkmobiles.com/blog/responsive-website-vs-mobile-app/.
+
+</p>
+</aside>
+<!-- End right col -->
+<?php include "includes/footer.php"?>
