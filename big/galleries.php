562e82794133215717530558c4721f3be628025c
Beverly James <jbevk94@gmail.com>
2020-02-17 15:19:52 -0800

    Added Gallery Page

---
+++ big/galleries.php
@@ -0,0 +1,26 @@
+<?php include "includes/header.php"?>
+
+<h1>Galleries</h1>
+<p>
+It’s pretty common to have images on your website since it is an easy and clear way to promote the goods or services provided on the site. Having a photo gallery can make any website more exciting as it’s a good way to integrate images throughout your site. There are a lot of upsides to having a lot of images on your website. Your website will get more views. (Leonov) It helps promote your business by attracting visitors and helping convert them into customers.  Having images make you seem more reliable since there’s proof of your work.
+</p> 
+<p>
+There are a ton of ways you can use a photo gallery on your site. A gallery can be added to your site using basic HTML, CSS, or JavaScript but you can also use a library or framework as well. Not everyone will be using your site on a computer so using media queries allows you to make your image gallery responsive so it will look its best regardless of the device used. 
+</p>
+<p>
+There are limitless possibilities when it comes to styling your gallery. You’re able to filter which pictures are visible at a time, include them in a slideshow, a carousel, or so much more. When applying an image gallery to your page you want to ensure that it is adding to the experience and not simply filling space. It’s important to use high-quality images. It’s possible to add captions to images, but I would recommend finding other ways to add the info or to use the captions sparingly because they can take away from the appeal. (Cousins, 2016) If you decide to use a carousel or a slider, I recommend cutting out elements that don’t seem necessary like the pause or back arrow.
+</p>
+</section>
+<!-- End left col -->
+
+<!-- Start right col -->
+<aside>
+<h2>Citations</h2>
+<p>
+Cousins, Carrie. “7 Tips for Better Responsive Photo Galleries.” Design Shack, Design Shack, 31 Aug. 2016, designshack.net/articles/layouts/photo-gallery-tips/.
+
+Leonov, Ievgen. “5 Ways to Use a Photo Gallery For Your Website.” The Blog, www.cincopa.com/blog/5-ways-to-use-a-photo-gallery-for-website/.
+</p>
+</aside>
+<!-- End right col -->
+<?php include "includes/footer.php"?>
