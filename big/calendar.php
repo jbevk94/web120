6b76a730c9fda1305203ac2890cc0f3a533e9fc5
Beverly James <jbevk94@gmail.com>
2020-02-17 16:16:19 -0800

    Added Calendar Page

---
+++ big/calendar.php
@@ -0,0 +1,14 @@
+<?php include "includes/header.php"?>
+
+<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23039BE5&amp;ctz=America%2FLos_Angeles&amp;showTitle=0&amp;showNav=1&amp;showPrint=0&amp;showTabs=1&amp;showCalendars=1&amp;showTz=1&amp;mode=WEEK" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
+</section>
+<!-- End left col -->
+
+<!-- Start left col -->
+<aside>
+<h3>Event Info</h3>
+<p>Add Event text here</p>
+</aside>
+
+<?php include "includes/footer.php"?>
