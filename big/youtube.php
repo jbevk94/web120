91bba01093c0c3a54b27f8c15e74698747ff87f3
Beverly James <jbevk94@gmail.com>
2020-02-17 16:17:26 -0800

    Added Youtube Page

---
+++ big/youtube.php
@@ -0,0 +1,16 @@
+<?php include "includes/header.php"?>
+
+<iframe width="420" height="315"
+src="https://www.youtube.com/watch?v=XQaHAAXIVg8">
+</iframe>
+
+</section>
+<!-- End left col -->
+
+<!-- Start left col -->
+<h3>HTML & CSS Crash Course Tutorial #8 - CSS Layout & Position by The Net Ninja</h3>
+
+<p>Explain what the video is about</p>
+<!-- End right col -->
+
+<?php include "includes/footer.php"?>
