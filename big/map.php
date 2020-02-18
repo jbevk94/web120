0e2d2b3693a0f1cab27294edc02ede089c2991dd
Beverly James <jbevk94@gmail.com>
2020-02-17 16:16:53 -0800

    Added Map Page

---
+++ big/map.php
@@ -0,0 +1,12 @@
+<?php include "includes/header.php"?>
+
+<div style="width: 100%"><iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=1701%20Broadway%2C%20Seattle%2C%20WA%2098122+(Map)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">gps coordinates finder</a></iframe></div><br />
+    <aside>
+        <h3>Contact Info</h3>
+        <p>Add Address</p>
+</aside>
+<!-- End right col -->
+
+<!-- Google Map API Key : AIzaSyDb9R4EKn2tHlA2Nq6T43VA8GDyMjSpE14 -->
+
+<?php include "includes/footer.php"?>
